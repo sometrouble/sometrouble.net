ef383c3974f2941932fdb6de42b154a73b3e5a72
sometrouble <sumeshi008@gmail.com>
2018-07-03 18:46:35 +0900

    [init] ka-chan

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template lang='pug'>
+  div#app
+    router-view
+</template>
+
+<script lang='coffee'>
+export default
+  name: 'App'
+</script>
+
+<style lang='scss'>
+body {
+  background-image: url(https://i.gyazo.com/23e4de1e8ff9efdb69db72448386b622.png); 
+  background-position: center center; 
+  background-repeat: no-repeat;
+  background-attachment: fixed; 
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template lang='pug'>
+h1.hello {{url}}
+</template>
+
+<script lang='coffee'>
+export default 
+  data: () ->
+    url: 'https://www.hellowork.go.jp/'
+</script>
+
+<style lang='scss' scoped>
+.hello {
+  margin-top: 50vh;
+}
+</style>
