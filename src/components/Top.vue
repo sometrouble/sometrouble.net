a805b2296d0ca1c875d7861cec2da768111010f8
sometrouble <Cellophan_tape@yahoo.co.jp>
2018-07-29 01:52:15 +0900

    [add] top

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,15 +1,21 @@
 <template lang='pug'>
-p ここがメインだぞおおおおおおおおおおおおおおおおおおおおおお
+  v-container.jumbotron(fill-height)
+    v-layout(align-center)
+      v-flex
+        h1.display-1 sometrouble.net
+        span.body-1 In solitude, where we are least alone.
+        v-divider.my-2
 </template>
 
 <script lang='coffee'>
-export default 
-  data: () ->
-    url: 'https://www.hellowork.go.jp/'
 </script>
 
 <style lang='scss' scoped>
-.hello {
-  margin-top: 50vh;
+.jumbotron {
+  min-width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-image: url("../assets/images/main_003.jpg");
 }
 </style>
