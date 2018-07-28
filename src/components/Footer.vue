c5088645ba354db6bf3cde890ab7ebc29230dcda
sometrouble <Cellophan_tape@yahoo.co.jp>
2018-07-29 01:51:53 +0900

    [update] footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,9 @@
 <template lang='pug'>
-p fuga
+footer
+    v-footer.pa-3(fixed)
+        v-flex(text-xs-center)
+            v-spacer
+            div &copy; {{ new Date().getFullYear() }} sometrouble.net
 </template>
 
 <script lang='coffee'>
